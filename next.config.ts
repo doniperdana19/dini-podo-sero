bed900e362db01dfb092855498f97645fe9a8bf5
doniperdana19 <doniperdana539@gmail.com>
2026-05-27 01:24:07 +0700

    fix: update next config formatting to valid typescript

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // Memaksa Vercel mengabaikan error type checking saat build
+    // Memaksa Vercel mengabaikan semua error type checking saat build production
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Memaksa Vercel mengabaikan pengecekan ESLint saat build
+    // Memaksa Vercel mengabaikan pengecekan ESLint saat build production
     ignoreDuringBuilds: true,
   },
 };
